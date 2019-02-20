ed78e04daa2bf5ad804e560258ff04634f3c2544
Prakasit <Prakasit@NB_PRAKASIT>
2019-02-21 00:28:54 +0700

    update ef

--- BIG.VMS.PRESENT/BIG.VMS.DAL/BIG_VMS_DB.Context.cs
+++ BIG.VMS.PRESENT/BIG.VMS.DAL/BIG_VMS_DB.Context.cs
@@ -27,5 +27,13 @@ namespace BIG.VMS.DAL
         }
     
         public DbSet<MEMBER_LOGON> MEMBER_LOGON { get; set; }
+        public DbSet<MAS_CAR_MODEL> MAS_CAR_MODEL { get; set; }
+        public DbSet<MAS_DEPARTMENT> MAS_DEPARTMENT { get; set; }
+        public DbSet<MAS_EMPLOYEE> MAS_EMPLOYEE { get; set; }
+        public DbSet<MAS_PROVINCE> MAS_PROVINCE { get; set; }
+        public DbSet<TRN_APPOINTMENT> TRN_APPOINTMENT { get; set; }
+        public DbSet<TRN_VISITOR> TRN_VISITOR { get; set; }
+        public DbSet<MAS_CAR_BRAND> MAS_CAR_BRAND { get; set; }
+        public DbSet<MAS_CAR_TYPE> MAS_CAR_TYPE { get; set; }
     }
 }
